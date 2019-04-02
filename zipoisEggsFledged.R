2b7c746f6d3dd93807bb3308c4e61bc38164dfc5
Francisco Lima <franciscolima@Franciscos-iMac-Pro.local>
2019-04-02 21:25:19 +0100

    update zipois, rm dordlogit

--- zipoisEggsFledged.R
+++ zipoisEggsFledged.R
@@ -6,8 +6,8 @@ library(magrittr)
 library(readxl)
 
 # Download data set from Riehl et al. 2019
-dataURL <- "https://datadryad.org/bitstream/handle/10255/dryad.204922/Riehl%20and%20Strong_Social%20Parasitism%20Data_2007-2017_DRYAD.xlsx"
-download.file(dataURL, destfile = "data.xlsx")
+# dataURL <- "https://datadryad.org/bitstream/handle/10255/dryad.204922/Riehl%20and%20Strong_Social%20Parasitism%20Data_2007-2017_DRYAD.xlsx"
+# download.file(dataURL, destfile = "data.xlsx")
 
 (allTabs <- excel_sheets("data.xlsx")) # list tabs
 
@@ -27,27 +27,49 @@ fro %<>% mutate(female_id = as.integer(factor(Female_ID_coded)),
                 group_id = as.integer(factor(Group_ID_coded)))
 
 # Filter out missingness in fledged eggs, the model does not cope with it
-fro %<>% slice(which(!is.na(Eggs_fledged)))
+fro %<>% slice(which(!is.na(Eggs_fledged))) %>% 
+      as.data.frame()
 
-eggsMod <- map2stan(alist(
+eggsFMod <- map2stan(alist(
       Eggs_fledged ~ dzipois(p, lambda),
-      logit(p) <- aP,
+      logit(p) <- ap,
       log(lambda) <- a + a_fem[female_id] + a_year[year_id] + a_group[group_id] +
-            Group_size*bE + Parasite * bP + Min_age * bA,
-      Group_size ~ dnorm(4.5, 2),
-      # APPARENTLY IMPUTATION ONLY WORKS WITH DNORM!
+            Parasite*bP + Min_age*bA + Group_size*bGS,
+      Group_size ~ dnorm(nuGS, 5),
+      nuGS ~ dnorm(4.5, 1),
       a_fem[female_id] ~ dnorm(0, sigma1),
       a_year[year_id] ~ dnorm(0, sigma2),
       a_group[group_id] ~ dnorm(0, sigma3),
       c(sigma1, sigma2, sigma3) ~ dcauchy(0, 1),
-      c(a, aP) ~ dnorm(0, 3),
-      c(bE, bP, bA, bE) ~ dnorm(0, 2)),
-      data = as.data.frame(fro), iter = 5e3, warmup = 1e3, chains = 4, cores = 4)
+      c(ap, a) ~ dnorm(0, 3),
+      c(bP, bA, bGS) ~ dnorm(0, 2)),
+      data = fro,
+      iter = 5e3, warmup = 1e3, chains = 4, cores = 4)
 
-precis(eggsMod)
+# Try Eggs_fledged ~ dzipois
+froReduced <- fro[!is.na(fro$Eggs_incu),]
+eggsHMod <- map2stan(alist(
+      Eggs_incu ~ dzipois(p, lambda),
+      logit(p) <- ap,
+      log(lambda) <- a + a_fem[female_id] + a_year[year_id] + a_group[group_id] +
+            Parasite*bP + Min_age*bA + Group_size*bGS,
+      Group_size ~ dnorm(nuGS, 5),
+      nuGS ~ dnorm(4.5, 1),
+      a_fem[female_id] ~ dnorm(0, sigma1),
+      a_year[year_id] ~ dnorm(0, sigma2),
+      a_group[group_id] ~ dnorm(0, sigma3),
+      c(sigma1, sigma2, sigma3) ~ dcauchy(0, 1),
+      c(ap, a) ~ dnorm(0, 3),
+      c(bP, bA, bGS) ~ dnorm(0, 2)),
+      data = froReduced,
+      iter = 5e3, warmup = 1e3, chains = 4, cores = 4)
+
+# Try predicting Eggs_laid later, produce eggsLMod
+precis(eggsFMod, prob = .95)
+precis(eggsHMod, prob = .95)
 
 # Sample posterior
-post <- extract.samples(eggsMod)
+post <- extract.samples(eggsFMod)
 
 # PI of P(no clutch at all)
-dens(logistic(post$aP))
+dens(logistic(post$ap))
