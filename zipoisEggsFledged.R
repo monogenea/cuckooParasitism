4ce8f3e215ee912800d028b4d1631f05ffadd7cc
Francisco Lima <franciscolima@Franciscos-iMac-Pro.local>
2019-04-04 19:21:49 +0100

    update sims with zipois models

--- zipoisEggsFledged.R
+++ zipoisEggsFledged.R
@@ -32,7 +32,8 @@ eggsFMod <- map2stan(alist(
       Eggs_fledged ~ dzipois(p, lambda),
       logit(p) <- ap,
       log(lambda) <- a + a_fem[female_id] + a_year[year_id] + a_group[group_id] +
-            Parasite*bP + Min_age*bA + Group_size*bGS + Mean_eggsize*bES,
+            Parasite*bP + Min_age*bA + Group_size*bGS + Mean_eggsize*bES +
+            Parasite*Min_age*bPA,
       Group_size ~ dnorm(nuGS, 5), # remove this, the hist is bimodal!
       Mean_eggsize ~ dnorm(nuES, 5), # 215 NAs!
       nuGS ~ dnorm(4.5, 1),
@@ -42,7 +43,7 @@ eggsFMod <- map2stan(alist(
       a_group[group_id] ~ dnorm(0, sigma3),
       c(sigma1, sigma2, sigma3) ~ dcauchy(0, 1),
       c(ap, a) ~ dnorm(0, 3),
-      c(bP, bA, bGS, bES) ~ dnorm(0, 2)),
+      c(bP, bA, bGS, bES, bPA) ~ dnorm(0, 2)),
       data = fro,
       iter = 5e3, warmup = 1e3, chains = 4, cores = 4)
 
@@ -60,8 +61,8 @@ eggsLMod <- map2stan(alist(
       log(lambda) <- a + a_fem[female_id] + a_year[year_id] + a_group[group_id] +
             Parasite*bP + Min_age*bA + Group_size*bGS + Mean_eggsize*bES +
             Parasite*Min_age*bPA,
-      Group_size ~ dnorm(nuGS, 5), # remove this, the hist is bimodal!
-      Mean_eggsize ~ dnorm(nuES, 5), # 215 NAs!
+      Group_size ~ dnorm(nuGS, 5),
+      Mean_eggsize ~ dnorm(nuES, 5),
       nuGS ~ dnorm(4.5, 1),
       nuES ~ dnorm(30, 2),
       a_fem[female_id] ~ dnorm(0, sigma1),
@@ -77,6 +78,46 @@ eggsLMod <- map2stan(alist(
 precis(eggsFMod, prob = .95) # use depth = 2 for varying intercepts
 precis(eggsLMod, prob = .95)
 
+# INFERENCE IN THE Eggs_fledged ~ dzipois MODEL
+# Sample posterior
+post <- extract.samples(eggsFMod)
+# PI of P(no clutch at all)
+dens(logistic(post$ap), show.HPDI = T, xlab = "ZIP Bernoulli(p)")
+
+# Run simulations (predictive posterior?)
+lambdaNoP <- exp(post$a + 0*post$bP + 6*post$bA +
+                       4*post$bGS + 30*post$bES + 0*6*post$bPA)
+simFledgeNoPar <- rpois(n = length(lambdaNoP), lambda = lambdaNoP)
+
+lambdaP <- exp(post$a + 1*post$bP + 6*post$bA +
+                     4*post$bGS + 30*post$bES + 6*post$bPA)
+simFledgePar <- rpois(n = length(lambdaP), lambda = lambdaP)
+table(simFledgeNoPar)
+table(simFledgePar)
+
+# Sim with varying group size
+rangeA <- seq(2, 14, length.out = 100)
+# No parasite
+predictions <- sapply(rangeA, function(x){
+      exp(post$a + 0*post$bP + x*post$bA + 6*post$bGS +
+                30*post$bES + 0*x*post$bPA)
+})
+hdpiPois <- apply(predictions, 2, HPDI, prob = .95)
+meanPois <- colMeans(predictions)
+plot(rangeA, meanPois, type = "l", ylim = c(0, 4),
+     xlab = "Min Age", ylab = expression(lambda))
+shade(hdpiPois, rangeA)
+# Parasite
+predictionsP <- sapply(rangeA, function(x){
+      exp(post$a + 1*post$bP + x*post$bA + 6*post$bGS +
+                30*post$bES + x*post$bPA)
+})
+hdpiPoisP <- apply(predictionsP, 2, HPDI, prob = .95)
+meanPoisP <- colMeans(predictionsP)
+lines(rangeA, meanPoisP, lty = 2)
+shade(hdpiPoisP, rangeA, col = rgb(1,0,0,.25))
+
+# INFERENCE IN THE Eggs_laid ~ dzipois MODEL
 # Sample posterior
 post <- extract.samples(eggsLMod)
 # PI of P(no clutch at all)
@@ -114,3 +155,4 @@ hdpiPoisP <- apply(predictionsP, 2, HPDI, prob = .95)
 meanPoisP <- colMeans(predictionsP)
 lines(rangeA, meanPoisP, lty = 2)
 shade(hdpiPoisP, rangeA, col = rgb(1,0,0,.25))
+
